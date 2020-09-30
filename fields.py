c3df99e1df3e4dbc4a361cedca834f40d90142d8
AbhishekBiswas521 <52816169+AbhishekBiswas521@users.noreply.github.com>
2020-09-30 21:35:09 +0530

    Update fields.py

--- fields.py
+++ fields.py
@@ -14,7 +14,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
+#    along with Inboxen.  If not, see <https://www.gnu.org/licenses/>.
 ##
 
 from django import forms
